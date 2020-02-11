bc2a2e6b141725b49a5392be422d55d1fb495d28
cpauvert <cpauvert@users.noreply.github.com>
2020-02-11 10:44:55 +0100

    Project in-phi-luence initiated. Test phase started

---
+++ in-phi-luence.R
@@ -0,0 +1,10 @@
+# Automatic generation of network of influences of philosophers
+# based on Wikipedia pages
+# PAUVERT Charlie
+# 2020-02-11
+
+
+library(rvest)
+
+# Test URL
+url<-"https://en.wikipedia.org/wiki/Thomas_Kuhn"
