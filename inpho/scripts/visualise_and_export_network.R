fd42ac12e0c0bd14aa3f67252816c7496fa99d80
cpauvert <cpauvert@users.noreply.github.com>
2020-12-16 15:06:18 +0100

    Improved visualisation for large network
    and added network export

--- inpho/scripts/visualise_and_export_network.R
+++ inpho/scripts/visualise_and_export_network.R
@@ -10,6 +10,10 @@ net <- as.matrix(read.delim(snakemake@input[[1]], header=F))
 net <- apply(trimws(net), 1:2, URLdecode)
 # Built network
 g <- graph_from_data_frame(net)
+# And export to graphml for future analyses
+write_graph(g, snakemake@output[["network"]], format = "graphml")
+
+# Visualisation
 data <- toVisNetworkData(g, idToLabel=F)
 data$nodes$title <- paste0("<p>", data$nodes$id, "</p>")
 
@@ -18,6 +22,8 @@ visNetwork(nodes = data$nodes, edges = data$edges, height = "500px",
                          vcount(g), " thinkers and ",
                          ecount(g), " influences.")) %>%
   visEdges(arrows = "to") %>% 
+  visNodes(size = 12) %>%
   visOptions(highlightNearest = list(enabled = T, hover = T)) %>% 
-  visSave(file = snakemake@output[[1]])
+  visIgraphLayout() %>%
+  visSave(file = snakemake@output[["html"]])
 
