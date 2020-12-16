f273e039c1dca5c5c4964d42d5abe1e61116c9cd
cpauvert <cpauvert@users.noreply.github.com>
2020-12-16 15:03:00 +0100

    Extend the network by using the 'was_influenced' properties (fixes #8)

--- inpho/scripts/get_influence_network.py
+++ inpho/scripts/get_influence_network.py
@@ -8,9 +8,14 @@ onto = get_ontology(snakemake.input[0]).load()
 
 # Search the philosophers with the influenced properties
 influencers = onto.search(has_influenced = "*")
+influencers_swap = onto.search(was_influenced = "*")
 with open(snakemake.output[0],"w") as network:
     for phil in influencers:
         for influencee in phil.has_influenced:
             print(phil.get_name(), "\t", influencee.get_name(),
+                    file=network)
+    for phil in influencers_swap:
+        for influencer in phil.was_influenced:
+            print(influencer.get_name(), "\t", phil.get_name(),
                     file=network)
 
